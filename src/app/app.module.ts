35b8c3e3b5b91aa5067b166136d6ad8c22b37d13
surajkarda <surajforlove@gmail.com>
2017-02-12 17:22:52 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { Page2}  from '../pages/page2/page2';
 
 @NgModule({
   declarations: [
@@ -22,3 +23,5 @@ import { Page2 } from '../pages/page2/page2';
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
+
+
--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -6,10 +6,11 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-page2',
   templateUrl: 'page2.html'
 })
-export class Page2 {
+export  class Page2 {
   selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
